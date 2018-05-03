f4790d39b9583261e65a6888e8cf1d8323f2b6db
Andrés Pérez <7andresperez@gmail.com>
2018-05-02 23:30:20 -0500

    Cambio de nombre de variable "inicial" por "contador"

--- cuadradoMagico.cpp
+++ cuadradoMagico.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 	int cuadradoMagico[3][3];
-	int inicial =1, opcion, k = 1;
+	int contador =1, opcion, k = 1;
 	cout<<"Este programa genera cuadrado magico de orden 3x3."<<endl;
 	//llenar la matriz de ceros
 	for (int a = 0; a < 3; a++) {
@@ -12,7 +12,7 @@ int main(){
 			cuadradoMagico[a][b] = 0;
 		}
 	}
-	cuadradoMagico[0][1] = inicial;
+	cuadradoMagico[0][1] = contador;
 
 	//imprime la matriz inicial
 	for (int a = 0; a < 3; a++) {
@@ -26,11 +26,11 @@ int main(){
 	//Inicio método siamés
 	int i = 0;
 	int j = 1;
-	while (k < 10) {
+	while (k < 10) {//k es la cantidad de numeros que se pueden almacenar en el cuadrado magico. 9 porque el cuadrado es de 3x3
 		//avance diagonal
 		int filas = i - 1;//comienza en la fila -1 (donde esta el primer numero)
 		int columnas = j + 1;//comienza en la columna 2 (donde está el primer numero)
-		inicial = inicial + 1; //siguiente numero en el cuadrado magico
+		contador = contador + 1; //siguiente numero en el cuadrado magico
 		if (filas < 0 && columnas < 3) {//if columna<0 && fila<3
 			opcion = 1;
 		}
@@ -53,24 +53,24 @@ int main(){
 		{
 			case 1:
 				filas = 2;
-				cuadradoMagico[filas][columnas] = inicial;
+				cuadradoMagico[filas][columnas] = contador;
 				break;
 			case 2:
 				columnas = 0;
-				cuadradoMagico[filas][columnas] = inicial;
+				cuadradoMagico[filas][columnas] = contador;
 				break;
 			case 3:
 				filas = -1 * filas;
 				columnas = columnas - 1;
-				cuadradoMagico[filas][columnas] = inicial;
+				cuadradoMagico[filas][columnas] = contador;
 				break;
 			case 4:
 				if (cuadradoMagico[filas][columnas] == 0) {
-					cuadradoMagico[filas][columnas] = inicial;
+					cuadradoMagico[filas][columnas] = contador;
 				}
 				else {
 					i++;
-					cuadradoMagico[i][j] = inicial;
+					cuadradoMagico[i][j] = contador;
 					filas = i;
 					columnas = j;
 				}
